8f73d559e4f25765c2b86dd8de1b13d77918ae9d
Magda Pleban <komandos2000@gmail.com>
2024-05-19 16:43:27 +0200

    BrowserRouter changed to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter  as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { PrimeReactProvider } from "primereact/api";
 
@@ -12,10 +12,10 @@ axios.defaults.baseURL= process.env.NODE_ENV === 'production' ? "https://api.mag
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <Router basename={"/korkomat24/"}>
+    <HashRouter basename={"/korkomat24/"}>
     <PrimeReactProvider>
         <App />
       </PrimeReactProvider>
-    </Router>
+    </HashRouter>
  // </React.StrictMode>
 );
