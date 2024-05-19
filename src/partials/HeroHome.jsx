1fd1285446b5b05ac70b3115b2157ec931327c39
Magda Pleban <komandos2000@gmail.com>
2024-05-19 17:35:02 +0200

    url changes for form component

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -18,7 +18,7 @@ function HeroHome() {
             </p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-amber-500 hover:bg-amber-600 w-full mb-4 sm:w-auto sm:mb-0" href="/form">
+                <a className="btn text-white bg-amber-500 hover:bg-amber-600 w-full mb-4 sm:w-auto sm:mb-0" href="/korkomat24/form">
                   Wchodzę!
                 </a>
               </div>
